9f0dda865418959c2321b49475054d16b38f4e37
Tom Wilson <twilson63@gmail.com>
2021-07-22 15:37:43 +0000

    feat (publishContent): start crawler (#5)

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 deno lint --ignore=arc
 deno fmt --check --ignore=arc
-deno test -A --unstable --no-check adapter_test.js lib/spider/publish-content_test.js
+deno test -A --unstable --no-check adapter_test.js lib/spider/publish-content_test.js lib/spider/publish-data_test.js
